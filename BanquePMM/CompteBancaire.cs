ddea5ba51eea1a7766c7850c8969b8d7e3af1569
moisemb <papemoussa.mbengue.11@gmail.com>
2026-01-08 15:01:18 +0000

    Add project files and restore information for BanqueTests

---
+++ BanquePMM/CompteBancaire.cs
@@ -0,0 +1,56 @@
+﻿namespace BanquePMM;
+    public class CompteBancaire
+    {
+        private readonly string? _nomClient;
+        private bool _bloqué = false;
+
+        private CompteBancaire() { }
+
+        public CompteBancaire(string nomClient, double solde)
+        {
+            _nomClient = nomClient;
+            Solde = solde;
+        }
+
+        public double Solde { get; private set; }
+
+        public void Débiter(double montant)
+        {
+            if (_bloqué)
+            {
+                throw new Exception("Compte Bloqué");
+            }
+
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(montant, other: Solde);
+            
+            if (montant <= 0)
+            {
+                throw new ApplicationException("Le montant retiré doit être Positif");
+            }
+
+            Solde -= montant; 
+        }
+
+        public void Créditer(double montant)
+        {
+            if (_bloqué)
+            {
+                throw new Exception("Compte Bloqué");
+            }
+
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(montant);
+            Solde += montant;
+        }
+
+        private void BloquerCompte() { _bloqué = true; }
+
+        private void DéBloquerCompte() { _bloqué = false; }
+
+        public static void Main()
+        {
+            var cb = new CompteBancaire(nomClient: "Pr Mamadou Samba Camara", solde: 500000);
+            cb.Créditer(montant: 1000000);
+            cb.Débiter(montant: 500000);
+            Console.WriteLine($"Solde disponible : {cb.Solde}");
+        }
+    }
---
+++ BanqueTests/CompteBancaireTests.cs
@@ -0,0 +1,218 @@
+﻿using BanquePMM;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BanqueTests
+{
+    [TestClass]
+    public class CompteBancaireTests
+    {
+        #region Tests de la méthode Débiter
+
+        [TestMethod]
+        public void VérifierDébitCompteCorrect()
+        {
+            // Arrange
+            const double soldeInitial = 500000;
+            const double montantDébit = 400000;
+            const double soldeAttendu = 100000;
+            var cb = new CompteBancaire(nomClient: "Pr Ibrahima Fall", soldeInitial);
+
+            // Act
+            cb.Débiter(montantDébit);
+
+            // Assert
+            Assert.AreEqual(soldeAttendu, cb.Solde, delta: 0.001, message: "Compte débité incorrectement");
+        }
+
+        [TestMethod]
+        public void DébiterMontantSupérieurAuSoldeLèveArgumentOutOfRangeException()
+        {
+            // Arrange
+            const double soldeInitial = 100000;
+            const double montantDébit = 200000;
+            var cb = new CompteBancaire(nomClient: "Pr Ibrahima Fall", soldeInitial);
+
+            // Act
+            void Action() => cb.Débiter(montantDébit);
+
+            // Assert
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(Action);
+        }
+
+        [TestMethod]
+        public void DébiterMontantNégatifSoulèveApplicationException()
+        {
+            // Arrange
+            const double soldeInitial = 500000;
+            const double montantDébit = -400000;
+            var cb = new CompteBancaire(nomClient: "Pr Ibrahima NGom", soldeInitial);
+
+            // Act
+            void Action() => cb.Débiter(montantDébit);
+
+            // Assert
+            var ex = Assert.Throws<ApplicationException>(Action);
+            Assert.AreEqual("Le montant retiré doit être Positif", ex.Message);
+        }
+
+        [TestMethod]
+        public void DébiterMontantZéroSoulèveApplicationException()
+        {
+            // Arrange
+            const double soldeInitial = 500000;
+            const double montantDébit = 0;
+            var cb = new CompteBancaire(nomClient: "Pr Ibrahima Fall", soldeInitial);
+
+            // Act
+            void Action() => cb.Débiter(montantDébit);
+
+            // Assert
+            var ex = Assert.Throws<ApplicationException>(Action);
+            Assert.AreEqual("Le montant retiré doit être Positif", ex.Message);
+        }
+
+        #endregion
+
+        #region Tests de la méthode Créditer
+
+        [TestMethod]
+        public void VérifierCréditCompteCorrect()
+        {
+            // Arrange
+            const double soldeInitial = 100000;
+            const double montantCrédit = 50000;
+            const double soldeAttendu = 150000;
+            var cb = new CompteBancaire(nomClient: "Pr Ibrahima Fall", soldeInitial);
+
+            // Act
+            cb.Créditer(montantCrédit);
+
+            // Assert
+            Assert.AreEqual(soldeAttendu, cb.Solde, delta: 0.001, message: "Compte crédité incorrectement");
+        }
+
+        [TestMethod]
+        public void CréditMontantNégatifSoulèveArgumentOutOfRangeException()
+        {
+            // Arrange
+            const double soldeInitial = 100000;
+            const double montantCrédit = -50000;
+            var cb = new CompteBancaire(nomClient: "Pr Ibrahima Fall", soldeInitial);
+
+            // Act
+            void Action() => cb.Créditer(montantCrédit);
+
+            // Assert
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(Action);
+        }
+
+        [TestMethod]
+        public void CréditMontantZéroSoulèveArgumentOutOfRangeException()
+        {
+            // Arrange
+            const double soldeInitial = 100000;
+            const double montantCrédit = 0;
+            var cb = new CompteBancaire(nomClient: "Pr Ibrahima Fall", soldeInitial);
+
+            // Act
+            void Action() => cb.Créditer(montantCrédit);
+
+            // Assert
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(Action);
+        }
+
+        #endregion
+
+        #region Tests de la méthode Virement
+
+        [TestMethod]
+        public void VérifierVirementEntreDeuxComptesCorrect()
+        {
+            // Arrange
+            const double soldeInitialSource = 500000;
+            const double soldeInitialDestination = 200000;
+            const double montantVirement = 150000;
+            const double soldeAttenduSource = 350000;
+            const double soldeAttenduDestination = 350000;
+            
+            var cbSource = new CompteBancaire(nomClient: "M. Dupont", soldeInitialSource);
+            var cbDestination = new CompteBancaire(nomClient: "Mme Martin", soldeInitialDestination);
+
+            // Act
+            Virement(cbSource, cbDestination, montantVirement);
+
+            // Assert
+            Assert.AreEqual(soldeAttenduSource, cbSource.Solde, delta: 0.001, message: "Solde source incorrect");
+            Assert.AreEqual(soldeAttenduDestination, cbDestination.Solde, delta: 0.001, message: "Solde destination incorrect");
+        }
+
+        [TestMethod]
+        public void VirementMontantSupérieurAuSoldeSoulèveArgumentOutOfRangeException()
+        {
+            // Arrange
+            const double soldeInitialSource = 100000;
+            const double soldeInitialDestination = 200000;
+            const double montantVirement = 150000;
+            
+            var cbSource = new CompteBancaire(nomClient: "M. Dupont", soldeInitialSource);
+            var cbDestination = new CompteBancaire(nomClient: "Mme Martin", soldeInitialDestination);
+
+            // Act
+            void Action() => Virement(cbSource, cbDestination, montantVirement);
+
+            // Assert
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(Action);
+        }
+
+        [TestMethod]
+        public void VirementMontantNégatifSoulèveApplicationException()
+        {
+            // Arrange
+            const double soldeInitialSource = 500000;
+            const double soldeInitialDestination = 200000;
+            const double montantVirement = -100000;
+            
+            var cbSource = new CompteBancaire(nomClient: "M. Dupont", soldeInitialSource);
+            var cbDestination = new CompteBancaire(nomClient: "Mme Martin", soldeInitialDestination);
+
+            // Act
+            void Action() => Virement(cbSource, cbDestination, montantVirement);
+
+            // Assert
+            var ex = Assert.Throws<ApplicationException>(Action);
+        }
+
+        [TestMethod]
+        public void VirementMontantZéroSoulèveApplicationException()
+        {
+            // Arrange
+            const double soldeInitialSource = 500000;
+            const double soldeInitialDestination = 200000;
+            const double montantVirement = 0;
+            
+            var cbSource = new CompteBancaire(nomClient: "M. Dupont", soldeInitialSource);
+            var cbDestination = new CompteBancaire(nomClient: "Mme Martin", soldeInitialDestination);
+
+            // Act
+            void Action() => Virement(cbSource, cbDestination, montantVirement);
+
+            // Assert
+            var ex = Assert.Throws<ApplicationException>(Action);
+        }
+
+        #endregion
+
+        #region Méthode auxiliaire Virement
+
+        /// <summary>
+        /// Effectue un virement d'un compte source vers un compte destination
+        /// </summary>
+        private void Virement(CompteBancaire compteSource, CompteBancaire compteDestination, double montant)
+        {
+            compteSource.Débiter(montant);
+            compteDestination.Créditer(montant);
+        }
+
+        #endregion
+    }
+}
